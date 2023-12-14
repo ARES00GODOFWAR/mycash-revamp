84f3478d983607977127893de0f2da8777e68ae2
Zachary Clarke <zachary.clarke@digicelgroup.com>
2023-12-14 09:36:29 -0500

    Created the jumbotron

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,15 @@
+import Jumbotron from "~/component/jumbotron/jumbotron";
+
+
+const Dashboard = () => {
+    return (
+        <>
+            <Jumbotron/>
+
+
+        </>
+
+    );
+};
+
+export default Dashboard;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,16 @@ import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
+        hurme: ["HurmeGeometricSans1", "sans-serif"],
       },
     },
   },
